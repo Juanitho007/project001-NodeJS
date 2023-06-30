d7eca7cece4611f58ad56f30779b40d6d28906a8
Juanitho007 <juan041008dyb@gmail.com>
2023-06-30 17:59:26 -0600

    projectNode-001

---
+++ src/models/User.js
@@ -0,0 +1,30 @@
+const { DataTypes } = require("sequelize");
+const sequelize = require("../utils/connection");
+const User = sequelize.define("user", {
+  first_name: {
+    type: DataTypes.STRING(100),
+    allowNull: false,
+  },
+  last_name: {
+    type: DataTypes.STRING(100),
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING(254),
+    allowNull: false,
+  },
+  password: {
+    type: DataTypes.STRING(16),
+    allowNull: false,
+  },
+  birthday: {
+    type: DataTypes.DATE,
+    allowNull: false,
+  },
+  image_url: {
+    type: DataTypes.TEXT,
+    allowNull: false,
+  },
+});
+
+module.exports = User;
